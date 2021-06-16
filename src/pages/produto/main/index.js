f54612bb54fbd677720b445644c13383e5fbe16e
Veridiana Santos <veridiana-santos@outlook.com>
2021-06-16 15:20:18 -0300

    primeiro commit

---
+++ src/pages/produto/main/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+export default class Main extends Component {
+    constructor(props) {
+        super(props);
+ 
+        this.state = {
+            produto: [],
+            erro: null
+        };
+    }
+ 
+    componentDidMount() {
+        fetch(`${process.env.REACT_APP_API_URL}`)
+            .then(produto =>
+                produto.json().then(produto => this.setState({ produto }))
+            )
+            .catch(erro => this.setState({ erro }));
+    }
+ 
+    render() {
+        const { produto } = this.state;
+ 
+        return (
+            <div className="product-list">
+                <Link to={`/criarProduto`}> <button type="button" class="btn btn-success">Novo</button> </Link>
+                <br /><br />
+ 
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Preço</th>
+                            <th scope="col">Cadastro</th>
+                            <th scope="col">Ativo</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {produto.map((produto, index) => (
+                            <tr>
+                                <th scope="row">{produto.id}</th>
+                                <td>{produto.nome}</td>
+                                <td>{produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+                                <td>{new Date(produto.dataCadastro).toLocaleDateString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit' })}</td>
+                                <td>{produto.ativo ? "Sim" : "Não"}</td>
+                                <td> <Link to={`/produtos/${produto.id}`}> <button type="button" class="btn btn-primary">Detalhes</button> </Link> </td>
+                                <td> <Link to={`/editarProduto/${produto.id}`}> <button type="button" class="btn btn-warning">Atualizar</button> </Link></td>
+                                <td> <Link to={`/deletarProduto/${produto.id}`}> <button type="button" class="btn btn-danger">Excluir</button> </Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
