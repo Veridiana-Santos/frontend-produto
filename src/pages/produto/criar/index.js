dd23a2cbca2e6b865e839a0ebde752549255b6c3
Veridiana Santos <veridiana-santos@outlook.com>
2021-06-16 20:23:14 -0300

    atualização

--- src/pages/produto/criar/index.js
+++ src/pages/produto/criar/index.js
@@ -126,7 +126,7 @@ class criarProduto extends Component {
     };
  
     handleSubmit = event => {
-        fetch("${process.env.REACT_APP_API_URL}", {
+        fetch(`${process.env.REACT_APP_API_URL}` , {
             method: "post",
             body: JSON.stringify(this.state.produto),
             headers: {
